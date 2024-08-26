d6ec4e9b0792733b643c8beb3e78b15ad907032e
Stephen Sviatko <ssviatko@gmail.com>
2024-08-26 15:05:27 -0700

    Fix typo

--- decodels.cc
+++ decodels.cc
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 {
 	if (argc != 3) {
-		std::cout << "usage: decodels <passphrase>> <message>" << std::endl;
+		std::cout << "usage: decodels <passphrase> <message>" << std::endl;
 		exit(0);
 	}
 	std::cout << ss::data::decode_little_secret(std::string(argv[1]), std::string(argv[2])) << std::endl;
